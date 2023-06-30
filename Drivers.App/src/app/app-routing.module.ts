d4a1e7f607e2291731802eb565e4939c0c350cdc
westerntc <veretax@gmail.com>
2023-06-30 13:16:01 -0400

    Updated drivers.component and app-routing to facilitate moving to the add form (rather than trying to do it on the same screen as the listing)

--- Drivers.App/src/app/app-routing.module.ts
+++ Drivers.App/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DriversComponent } from './components/drivers/drivers.component';
 import { DriverDetailsComponent } from './components/driver-details/driver-details.component';
+import { DriverAddFormComponent } from './components/driver-add-form/driver-add-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/drivers', pathMatch: 'full' },
   { path: 'drivers', component: DriversComponent },
   { path: 'drivers/:id', component: DriverDetailsComponent },
+  { path: 'add-driver', component: DriverAddFormComponent },
 ];
 
 
--- Drivers.App/src/app/components/drivers/drivers.component.ts
+++ Drivers.App/src/app/components/drivers/drivers.component.ts
@@ -1,31 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Driver } from '../../models/driver';
 import { DriverService } from '../../services/driver.service'
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-drivers',
   templateUrl: './drivers.component.html',
   styleUrls: ['./drivers.component.css']
 })
-export class DriversComponent {
 
+export class DriversComponent implements OnInit {
   drivers: Driver[] = [];
   selectedDriver: Driver | undefined;
 
-  constructor(private driverService: DriverService, private router: Router) {
-
-  }
+  constructor(private driverService: DriverService, private router: Router) { }
 
   ngOnInit(): void {
-    this.driverService.getDrivers().subscribe((result: Driver[]) => (this.drivers = result));
+    this.loadDrivers();
+  }
+
+  loadDrivers(): void {
+    this.driverService.getDrivers().subscribe((result: Driver[]) => {
+      this.drivers = result;
+    });
   }
 
   showDriverDetails(driver: Driver): void {
     this.selectedDriver = driver;
   }
 
-  goBack(): void {
-    this.selectedDriver = undefined;
+  goToAddForm(): void {
+    this.router.navigate(['/add-driver']);
   }
 }
+//export class DriversComponent implements OnInit {
+
+//  drivers: Driver[] = [];
+//  selectedDriver: Driver | undefined;
+
+//  constructor(private driverService: DriverService, private router: Router) {
+
+//  }
+
+//  ngOnInit(): void {
+//    this.driverService.getDrivers().subscribe((result: Driver[]) => (this.drivers = result));
+//  }
+
+//  showDriverDetails(driver: Driver): void {
+//    this.selectedDriver = driver;
+//  }
+
+//  goBack(): void {
+//    this.selectedDriver = undefined;
+//  }
+
+//  goToAddForm(): void {
+//    this.router.navigate(['/add-driver']);
+//  }
+//}
