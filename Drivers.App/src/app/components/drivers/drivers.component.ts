ff17e6e0d775be1f7fb503435442fa7b3f1ddc66
westerntc <veretax@gmail.com>
2023-06-30 14:31:43 -0400

    added routing to driver-add-form Fixed successful from submission response to navigate to drivers/:id when completed Added onCancelButton to return to the drivers view
    Added styles for the buttons to position them better on the form.
    Added Styles to increase spacing, slightly for each form row.

--- Drivers.App/src/app/components/drivers/drivers.component.ts
+++ Drivers.App/src/app/components/drivers/drivers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Driver } from '../../models/driver';
-import { DriverService } from '../../services/driver.service'
+import { DriverService } from '../../services/driver.service';
+
 
 
 @Component({
@@ -32,30 +33,14 @@ export class DriversComponent implements OnInit {
 
   goToAddForm(): void {
     this.router.navigate(['/add-driver']);
+
+    // Note that while testing Cancel on add-driver I discovered
+    // that it wasn't loading, so I thought by calling .then
+    // after it was done with the navigate could do this.
+    // However, it turns out, at the moment to not be necessary,
+    // because getDrivers is called in ngOnInit
+    //this.router.navigate(['/add-driver']).then(() => {
+    //  this.loadDrivers(); // Refresh the drivers after navigating back
+    //});
   }
 }
-//export class DriversComponent implements OnInit {
-
-//  drivers: Driver[] = [];
-//  selectedDriver: Driver | undefined;
-
-//  constructor(private driverService: DriverService, private router: Router) {
-
-//  }
-
-//  ngOnInit(): void {
-//    this.driverService.getDrivers().subscribe((result: Driver[]) => (this.drivers = result));
-//  }
-
-//  showDriverDetails(driver: Driver): void {
-//    this.selectedDriver = driver;
-//  }
-
-//  goBack(): void {
-//    this.selectedDriver = undefined;
-//  }
-
-//  goToAddForm(): void {
-//    this.router.navigate(['/add-driver']);
-//  }
-//}
